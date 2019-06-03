7fc02bde56755d3a7fb1ae3315ba3aa842151e5d
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2019-06-03 10:05:00 -0500

    Refactored disabilityAmount()

--- src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
+++ src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
@@ -1,13 +1,9 @@
 double disabilityAmount() {
-  if (seniority < 2) {
-    return 0;
-  }
-  if (monthsDisabled > 12) {
-    return 0;
-  }
-  if (isPartTime) {
-    return 0;
-  }
+    final boolean isSenior = seniority >= 2;
+    final boolean yearDisabled = monthsDisabled > 12;
+
+    if (!isSenior || yearDisabled || isPartTime) { return 0 }
+
   // Compute the disability amount.
   // ...
 }
