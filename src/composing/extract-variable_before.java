5773805aae3b8dbedc438ccc4537381878454271
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2019-06-03 09:44:48 -0500

    Refactored renderBanner()

--- src/composing/extract-variable_before.java
+++ src/composing/extract-variable_before.java
@@ -1,8 +1,7 @@
 void renderBanner() {
-  if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-       (browser.toUpperCase().indexOf("IE") > -1) &&
-        wasInitialized() && resize > 0 )
-  {
+    final boolean MACExists = platform.toUpperCase().indexOf("MAC") > -1;
+    final boolean IEExists = browser.toUpperCase().indexOf("IE") > -1;
+    if (MACExists && IEExists && wasInitialized() && resize > 0) {
     // do something
-  }
+    }
 }
